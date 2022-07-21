aa34d60cca846312f3c75e61451f0e170bcdf48a
Robert C. Kowalczyk <catdogramb@gmail.com>
2022-07-21 17:22:17 -0400

    Revert "Attempting to update Harmony library"
    This reverts commit 7790bd518f055904b1f21a2fd93ed171d962a9c9.

--- Patcher.cs
+++ Patcher.cs
@@ -23,8 +23,7 @@ namespace TollPriceIncreaserMod
             patched = true;
             Debug.Log("TollPriceIncreaser Patching");
             var harmony = new Harmony(HarmonyId);
-            //harmony.PatchAll(Assembly.GetExecutingAssembly());
-            harmony.PatchAll(typeof(Patcher).GetType().Assembly);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
 
         public static void UnpatchAll()
